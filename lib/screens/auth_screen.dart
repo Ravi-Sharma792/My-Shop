74da8ca92a1a35945914794cf1b005558d439dd1
Ravi-Sharma792 <ravi14092001@gmail.com>
2020-05-23 22:09:38 +0530

    screenshots

--- lib/screens/auth_screen.dart
+++ lib/screens/auth_screen.dart
@@ -242,6 +242,7 @@ class _AuthCardState extends State<AuthCard>
                     if (value.isEmpty || !value.contains('@')) {
                       return 'Invalid email!';
                     }
+                    return null;
                   },
                   onSaved: (value) {
                     _authData['email'] = value;
@@ -255,6 +256,7 @@ class _AuthCardState extends State<AuthCard>
                     if (value.isEmpty || value.length < 5) {
                       return 'Password is too short!';
                     }
+                    return null;
                   },
                   onSaved: (value) {
                     _authData['password'] = value;
@@ -282,6 +284,7 @@ class _AuthCardState extends State<AuthCard>
                                 if (value != _passwordController.text) {
                                   return 'Passwords do not match!';
                                 }
+                                return null;
                               }
                             : null,
                       ),
